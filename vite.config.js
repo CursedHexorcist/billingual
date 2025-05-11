c113627d0ceefa02f4b9a0cdab7b9876ec61fd43
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-11 22:03:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -12,9 +13,9 @@ export default defineConfig({
         'styled-components',
         '@emotion/react',
         '@emotion/styled',
-        'aos',
-        'swiper',  // jika ada error terkait dengan modul lainnya
-        'react-slick',  // misalnya jika react-slick digunakan
+        'swiper',
+        'react-slick',
+        // Tambahkan modul-modul lain yang mungkin menyebabkan masalah
       ],
     },
   },
