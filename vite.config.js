cf81a3dd7bf03c765a10fc37fb0f6af4bd791966
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-17 14:14:35 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      // alias kalau perlu
-    },
+    alias: {}
   },
   build: {
     chunkSizeWarningLimit: 600,
     rollupOptions: {
-      external: ['react', 'react-dom']  // tandai react dan react-dom sebagai external
+      onwarn(warning, warn) {
+        // Abaikan warning 'this is likely unintended external'
+        if (
+          warning.message &&
+          warning.message.includes('This is most likely unintended because it can break your application')
+        ) {
+          return
+        }
+        warn(warning)
+      },
+      external: [], // kosongkan dulu, jangan pakai external sembarangan
     }
-  },
+  }
 })
+
