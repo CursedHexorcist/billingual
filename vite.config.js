5b3e52c048601c978a5ab56175649f3412d891c2
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-17 13:24:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        'react/jsx-runtime',
-        'styled-components',
-        '@emotion/react',
-        '@emotion/styled',
-        'swiper',
-        'react-slick',
-        'react-swipeable-views', // ← Tambahkan ini
-      ],
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor_react'
+            }
+            if (id.includes('swiper')) {
+              return 'vendor_swiper'
+            }
+            if (id.includes('react-slick')) {
+              return 'vendor_react_slick'
+            }
+            if (id.includes('react-swipeable-views')) {
+              return 'vendor_swipeable_views'
+            }
+            // Semua modul node_modules lainnya masuk ke 'vendor'
+            return 'vendor'
+          }
+        },
+      },
     },
+    chunkSizeWarningLimit: 600, // naikkan limit warning agar build tidak terlalu banyak peringatan
   },
-});
+})
